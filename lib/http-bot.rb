d570ac73fbbc6e30956a914d9022e15dce15442c
root <root@centos.(none)>
2008-07-31 11:17:04 -0500

    Added response_headers method

--- lib/http-bot.rb
+++ lib/http-bot.rb
@@ -5,11 +5,12 @@ module HTTPBot
 
   #Use this to login and access one particular site
   def self.connect(host,options={})
-    yield Connection.new(host)
+    yield Connection.new(host) if block_given?
   end
 	
   class Connection
-
+    attr_reader :response_headers, :response, :redirected_to
+    
     def initialize(host,options={})
       @host = host
       @user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.1) Gecko/2008070208 Firefox/3.0.1"
@@ -17,6 +18,9 @@ module HTTPBot
       @url  = URI.parse(@host)
       @http = Net::HTTP.new(@url.host, @url.port)
       @http.use_ssl = @host =~ /^https/i ? true : false
+      @response_header = {}
+      @response = nil
+      @redirected_to = nil
       
       fetch_cookies
     end
@@ -26,31 +30,44 @@ module HTTPBot
       get { |http, response| set_cookies(response) }
     end
     
-    def request(url,type='Get')
+    def request(url,type='Get',form_data={})
       url = "/#{url}" unless url =~ /^\//
       @url  = URI.parse(@host)
       @url.path = url
       klass = eval("Net::HTTP::#{type}")
       req = klass.new(@url.path)
+      
       req.add_field('User-Agent',@user_agent)
+      @cookies.each { |cookie| req.add_field('Cookie',cookie) }
+      req.set_form_data(form_data)
+      
       res = @http.start do |http| 
-        response = http.request(req)
-	yield(http,response)
+        @response = response = http.request(req)
+	set_response_headers(response)
+	yield(http,response) if block_given?
       end
     end
     
-    def get(url='',&block)
-      request(url,'Get',&block)
+    def get(url='',form_data={},&block)
+      request(url,'Get',form_data,&block)
     end
     
-    def post(url='',&block)
-      request(url,'Post',&block)
+    def post(url='',form_data={},&block)
+      request(url,'Post',form_data,&block)
     end
     
     def set_cookies(response)
       @cookies = []
       response.each_header do |key,val|
         @cookies << val if key =~ /set-cookie/i
+      end
+    end
+    
+    def set_response_headers(response)
+      @response_headers = {}
+      response.each_header do |key,val| 
+        @response_headers[key] = val 
+        @redirected_to = val if key =~ /location/i
       end
     end
     
